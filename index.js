87b9c9c5081827c71f70519d593c0ea1162c1597
duqings <duqings@foxmail.com>
2024-07-01 11:22:13 +0800

    feat: 🎉 NPM 发包的一些相关尝试

---
+++ index.js
@@ -0,0 +1,11 @@
+/*
+ * @Description: 
+ * @Author: duqings duqings@foxmail.com
+ * @Date: 2024-07-01 11:17:52
+ * @LastEditors: duqings duqings@foxmail.com
+ * @LastEditTime: 2024-07-01 11:19:47
+ */
+
+
+'use strict';
+module.exports = require('./Logger');
