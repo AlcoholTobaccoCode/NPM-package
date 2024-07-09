7f330e8793f5f6fe6e7739d17b9384f5fc8ef37f
duqings <duqings@foxmail.com>
2024-07-10 00:52:43 +0800

    refactor: ♻️ 推翻重来

---
+++ index.js
@@ -0,0 +1,13 @@
+/*
+ * @Description: 统一导出
+ * @Author: duqings duqings@foxmail.com
+ * @Date: 2024-07-10 00:36:38
+ * @LastEditors: duqings duqings@foxmail.com
+ * @LastEditTime: 2024-07-10 00:50:28
+ */
+
+import Logger from './lib/Logger';
+
+export {
+  Logger
+}
